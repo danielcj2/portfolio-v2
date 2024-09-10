7c38ce432f5907fdad966813c0ce07a7731bf32f
Daniel Cojocea <danielcojocea28@gmail.com>
2024-09-09 20:52:19 -0400

    Added framer motion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,26 @@
 import { useLenis } from "lenis/react";
+import { useState } from "react";
+import { useMotionValueEvent, useScroll } from "framer-motion";
 import Logo from "../assets/icons/logo.svg?react";
 
 const Header = () => {
   const lenis = useLenis();
+  const { scrollY } = useScroll();
+  const [scrolled, setScrolled] = useState(false);
 
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    latest > 150 ? setScrolled(true) : setScrolled(false);
+  });
+
+  /** Takes a `link` prop which is used to create a link that smoothly scrolls
+   * to the corresponding section when clicked. **/
   const NavLink = ({ link }) => {
+    let hashed = `#${link}`;
     return (
       <a
-        href={`/#${link}`}
-        className="capitalize"
-        onClick={() => lenis.scrollTo(`#${link}`)}
+        href={hashed}
+        className="text-sm uppercase"
+        onClick={() => lenis.scrollTo(hashed)}
       >
         {link}
       </a>
@@ -17,7 +28,7 @@ const Header = () => {
   };
 
   return (
-    <div className="fixed flex w-full justify-between px-10 py-7">
+    <div className="fixed flex w-full items-start justify-between px-10 py-7">
       <a
         href="#hero"
         onClick={() => lenis.scrollTo("#hero")}
@@ -25,7 +36,9 @@ const Header = () => {
       >
         <Logo className="h-full w-full" />
       </a>
-      <nav className="flex gap-3">
+      <nav
+        className={`flex flex-col gap-2 text-right ${scrolled ? "bg-bg-dark" : ""} mr-[-0.75rem] rounded-lg p-3`}
+      >
         <NavLink link="about" />
         <NavLink link="skills" />
         <NavLink link="work" />
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       className="flex h-screen w-full flex-1 flex-col justify-between px-8 py-5"
     >
       <h1 className="text-center font-geist-mono">Hello there</h1>
-      <div className="flex flex-row items-end gap-5 self-end px-3">
+      <div className="flex flex-row items-end gap-5 self-end p-3">
         <Timestamp />
       </div>
     </section>
